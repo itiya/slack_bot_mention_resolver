ac8e1f3ea0b6b1ccc32d7b065b38bf2f12f94123
itiya <fragment.vertex@gmail.com>
2018-07-22 16:36:50 +0900

    [add] トークンに紐づくIDを取得

--- lib/slack_bot_mention_resolver.rb
+++ lib/slack_bot_mention_resolver.rb
@@ -1,5 +1,14 @@
 require "slack_bot_mention_resolver/version"
+require "slack"
 
 module SlackBotMentionResolver
-  # Your code goes here...
+  class Resolver
+    def initialize(token)
+      @client = ::Slack::Client.new(token: token)
+    end
+
+    def get_self_id
+      @id ||= @client.auth_test['user_id']
+    end
+  end
 end
