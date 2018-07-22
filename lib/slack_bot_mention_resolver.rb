c45b72e99e0ee9a1277cd8a40db77067db8c6d9e
itiya <fragment.vertex@gmail.com>
2018-07-22 16:00:25 +0900

    [init] Initial commit

---
+++ lib/slack_bot_mention_resolver.rb
@@ -0,0 +1,5 @@
+require "slack_bot_mention_resolver/version"
+
+module SlackBotMentionResolver
+  # Your code goes here...
+end
---
+++ lib/slack_bot_mention_resolver/version.rb
@@ -0,0 +1,3 @@
+module SlackBotMentionResolver
+  VERSION = "0.1.0"
+end
